c1c1df498c574f4840b1648957c70d38f28e370c
lidq <magicworldos@126.com>
2015-09-25 16:33:33 +0800

    顺序线性表

--- chapter.01/main.c
+++ chapter.01/main.c
@@ -5,8 +5,22 @@
  *      Author: lidq
  */
 
-
 #include <stdio.h>
+
+typedef struct
+{
+	//起始地址
+	void *addr;
+	//元素个数
+	int count;
+	//顺序线性表大小
+	int size;
+} List;
+
+int init_list(List *list)
+{
+	return 0;
+}
 
 int main(int argc, char **args)
 {
