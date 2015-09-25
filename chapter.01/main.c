967493f628be8dd3d00f1f2f4b7e98bd6ffdc8e8
lidq <magicworldos@126.com>
2015-09-25 15:50:08 +0800

    顺序线性表

---
+++ chapter.01/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 25, 2015
+ *      Author: lidq
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **args)
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
