b73da5f5e7f7dc453f133abda8d6f5ac9dc6fee8
lidq <lideqiang83@163.com>
2015-11-23 15:51:08 +0800

    多重邻接表

---
+++ chapter.06/e.g.6.4/graph.c
@@ -0,0 +1,217 @@
+/*
+ * graph.c
+ *
+ *  Created on: Nov 20, 2015
+ *      Author: lidq
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
+
+//初始化图
+bool graph_init(s_graph *graph, int vertex_size, void (*visit_vertex)(), void (*visit_arccell)())
+{
+	if (graph == null)
+	{
+		return false;
+	}
+
+	if (vertex_size <= 0)
+	{
+		return false;
+	}
+
+	//设置顶点个数
+	graph->size = vertex_size;
+	//申请顶点内存
+	graph->vertex = (s_vertex *) malloc(sizeof(s_vertex) * vertex_size);
+
+	//初始化顶点
+	for (int i = 0; i < vertex_size; i++)
+	{
+		graph->vertex[i].data = null;
+		graph->vertex[i].arccel_first = null;
+	}
+
+	//显示顶点数据的回调函数
+	graph->visit_vertex = visit_vertex;
+	//显示边数据的回调函数
+	graph->visit_arccell = visit_arccell;
+
+	return true;
+}
+
+//销毁图
+bool graph_destroy(s_graph *graph)
+{
+	if (graph == null)
+	{
+		return false;
+	}
+
+	free(graph->vertex);
+
+	return true;
+}
+
+/*
+ * 设置顶点数据
+ * int index  : 顶点索引号
+ * void *data : 数据指针
+ */
+bool graph_set_vertex(s_graph *graph, int index, void *data)
+{
+	if (graph == null)
+	{
+		return false;
+	}
+
+	graph->vertex[index].data = data;
+
+	return true;
+}
+
+/*
+ * 设置边或边内存的权重
+ * int i_index : 顶点i索引
+ * int j_index : 顶点j索引
+ * int weight   : 权重
+ * void *data   : 数据指针
+ */
+bool graph_insert_arccell(s_graph *graph, int i_index, int j_index, int weight, void *data)
+{
+	if (graph == null)
+	{
+		return false;
+	}
+
+	if (i_index < 0 || i_index >= graph->size)
+	{
+		return false;
+	}
+
+	if (j_index < 0 || j_index >= graph->size)
+	{
+		return false;
+	}
+
+	/***
+	 * 出入边
+	 */
+	//新建一个边
+	s_arccell *arccel = (s_arccell *) malloc(sizeof(s_arccell));
+	//边数据
+	arccel->data = data;
+	//权重
+	arccel->weight = weight;
+	//边尾关联的顶点索引号
+	arccel->i_index = i_index;
+	arccel->j_index = j_index;
+	arccel->next_i = null;
+	arccel->next_j = null;
+
+	//如果顶点中第一条边为空
+	if (graph->vertex[i_index].arccel_first == null)
+	{
+		//设置顶点的每一条边
+		graph->vertex[i_index].arccel_first = arccel;
+	}
+	else
+	{
+		//找到顶点弧中最后一条弧
+		s_arccell *p = graph->vertex[i_index].arccel_first;
+		while (p->next_i != null)
+		{
+			p = p->next_i;
+		}
+		if (p->i_index == i_index)
+		{
+			//追加新弧
+			p->next_i = arccel;
+		}
+	}
+
+	if (graph->vertex[j_index].arccel_first == null)
+	{
+		//设置顶点的每一条边
+		graph->vertex[j_index].arccel_first = arccel;
+	}
+	else
+	{
+		//找到顶点弧中最后一条弧
+		s_arccell *p = graph->vertex[j_index].arccel_first;
+		while (p->next_j != null)
+		{
+			p = p->next_j;
+		}
+		if (p->j_index == j_index)
+		{
+			//追加新弧
+			p->next_j = arccel;
+		}
+	}
+
+	return true;
+}
+
+//显示图信息
+bool graph_visit(s_graph *graph)
+{
+	if (graph == null)
+	{
+		return false;
+	}
+
+	//循环顶点
+	for (int i = 0; i < graph->size; i++)
+	{
+		//显示顶点索引及数据
+		printf("[%d, ", i);
+		graph->visit_vertex(graph->vertex[i].data);
+		printf("]");
+		s_arccell *p = graph->vertex[i].arccel_first;
+		if (p != null)
+		{
+			printf(" -> (%d, %d, %d, ", p->i_index, p->j_index, p->weight);
+			graph->visit_arccell(p->data);
+			printf(") ");
+		}
+		printf("\n");
+	}
+
+	printf("\n\n");
+
+	//循环顶点
+	for (int i = 0; i < graph->size; i++)
+	{
+		//显示顶点索引及数据
+		printf("[%d, ", i);
+		graph->visit_vertex(graph->vertex[i].data);
+		printf("]");
+
+		//显示顶点出度边的权重及数据
+		s_arccell *p = graph->vertex[i].arccel_first;
+		while (p != null)
+		{
+			printf(" --> {%d, %d, %d, ", p->i_index, p->j_index, p->weight);
+			graph->visit_arccell(p->data);
+
+			s_arccell *q = p->next_j;
+			while (q != null)
+			{
+				printf(" -> (%d, %d, %d, ", q->i_index, q->j_index, q->weight);
+				graph->visit_arccell(q->data);
+				printf(") ");
+				q = q->next_j;
+			}
+			p = p->next_i;
+			printf("} ");
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	return true;
+}
---
+++ chapter.06/e.g.6.4/graph.h
@@ -0,0 +1,77 @@
+/*
+ * graph.h
+ *
+ *  Created on: Nov 20, 2015
+ *      Author: lidq
+ */
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include "typedef.h"
+
+//边或边
+typedef struct s_arccell
+{
+	//权重
+	int weight;
+	//边头顶点索引号i vertex index
+	int i_index;
+	//边尾顶点索引号j vertex index
+	int j_index;
+	//数据
+	void *data;
+	//下一个与i相关的边结节
+	struct s_arccell *next_i;
+	//下一个与j相关的边结节
+	struct s_arccell *next_j;
+
+} s_arccell;
+
+typedef struct
+{
+	//顶点数据
+	void *data;
+	//第一条与此顶点相关的边
+	s_arccell *arccel_first;
+} s_vertex;
+
+//多重邻接表
+typedef struct
+{
+	//顶点数组
+	s_vertex *vertex;
+	//顶点个数
+	int size;
+	//显示顶点数据的回调函数
+	void (*visit_vertex)();
+	//显示边数据的回调函数
+	void (*visit_arccell)();
+} s_graph;
+
+//初始化图
+bool graph_init(s_graph *graph, int vertex_size, void (*visit_vertex)(), void (*visit_arccell)());
+
+//销毁图
+bool graph_destroy(s_graph *graph);
+
+/*
+ * 设置顶点数据
+ * int index  : 顶点索引号
+ * void *data : 数据指针
+ */
+bool graph_set_vertex(s_graph *graph, int index, void *data);
+
+/*
+ * 设置边或边内存的权重
+ * int i_index : 顶点i索引
+ * int j_index : 顶点j索引
+ * int weight   : 权重
+ * void *data   : 数据指针
+ */
+bool graph_insert_arccell(s_graph *graph, int i_index, int j_index, int weight, void *data);
+
+//显示图信息
+bool graph_visit(s_graph *graph);
+
+#endif /* GRAPH_H_ */
