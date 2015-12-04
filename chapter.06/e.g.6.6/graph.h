93d83dbfaa224bdde542e24e2ce85a4a32861b98
lidq <lideqiang83@163.com>
2015-12-04 10:38:45 +0800

    广度优先搜索

---
+++ chapter.06/e.g.6.6/graph.h
@@ -0,0 +1,94 @@
+/*
+ * graph.h
+ *
+ *  Created on: Nov 20, 2015
+ *      Author: lidq
+ */
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include "typedef.h"
+#include "queue.h"
+
+//边
+typedef struct s_arccell
+{
+	//权重
+	int weight;
+	//边头顶点索引号i vertex index
+	int i_index;
+	//边尾顶点索引号j vertex index
+	int j_index;
+	//数据
+	void *data;
+	//下一个与i相关的边结节
+	struct s_arccell *next_i;
+	//下一个与j相关的边结节
+	struct s_arccell *next_j;
+
+} s_arccell;
+
+typedef struct
+{
+	//顶点数据
+	void *data;
+	//第一条与此顶点相关的边
+	s_arccell *arccel_first;
+} s_vertex;
+
+//多重邻接表
+typedef struct
+{
+	//顶点数组
+	s_vertex *vertex;
+	//顶点个数
+	int size;
+	//显示顶点数据的回调函数
+	void (*visit_vertex)();
+	//显示边数据的回调函数
+	void (*visit_arccell)();
+	s_arccell **arccel_temp;
+	s_arccell **arccel_temp2;
+} s_graph;
+
+//初始化图
+bool graph_init(s_graph *graph, int vertex_size, void (*visit_vertex)(), void (*visit_arccell)());
+
+//销毁图
+bool graph_destroy(s_graph *graph);
+
+/*
+ * 设置顶点数据
+ * int index  : 顶点索引号
+ * void *data : 数据指针
+ */
+bool graph_set_vertex(s_graph *graph, int index, void *data);
+
+/*
+ * 设置边或边内存的权重
+ * int i_index : 顶点i索引
+ * int j_index : 顶点j索引
+ * int weight   : 权重
+ * void *data   : 数据指针
+ */
+bool graph_insert_arccell(s_graph *graph, int i_index, int j_index, int weight, void *data);
+
+//深度优先搜索
+bool graph_depth_first_search(s_graph *graph);
+
+//深度优先访问顶点递归算法
+void graph_dfs(s_graph *graph, int v_ind, bool *visited);
+
+//广度优先搜索
+bool graph_breadth_first_search(s_graph *graph);
+
+//广度优先访问顶点递归算法
+void graph_bfs(s_graph *graph, int v_ind, bool *visited, s_queue *queue);
+
+//显示图信息
+bool graph_visit(s_graph *graph);
+
+void free_int(int *i);
+
+#endif /* GRAPH_H_ */
---
+++ chapter.06/e.g.6.6/queue.h
@@ -0,0 +1,46 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#include "typedef.h"
+
+//节点
+typedef struct s_node
+{
+	void *data;
+	struct s_node *next;
+} s_node;
+
+//队列数据结构
+typedef struct
+{
+	//元素大小
+	int ele_size;
+	//头节点
+	s_node *header;
+	//脚节点
+	s_node *footer;
+	//用于释放节点数据的回调函数
+	void (*free_data)();
+	//用于访问节点数据内容的回调函数
+	void (*visit_data)(void *data);
+} s_queue;
+
+//构建一个队列
+bool init_queue(s_queue *queue, int ele_size, void (*free_data)(), void (*visit_data)());
+
+//销毁队列
+bool destroy_queue(s_queue *queue);
+
+//清空队列中所有元素
+bool clear_queue(s_queue *queue);
+
+//在队列尾追加一个元素
+bool queue_in(s_queue *queue, void *e);
+
+//在队列头删除一个元素
+bool queue_out(s_queue *queue, void **e);
+
+//对每个元素执行visit操作
+bool queue_visit(s_queue *queue);
+
+#endif
