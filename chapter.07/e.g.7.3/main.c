11ac49c6b9f8eb9b317fab2b998a06029826f350
lidq <lidqmagic@126.com>
2016-01-18 11:06:14 +0800

    二叉排序树

--- chapter.07/e.g.7.3/main.c
+++ chapter.07/e.g.7.3/main.c
@@ -10,15 +10,15 @@ int main(int argc, char **args)
 	tree_init(&tree);
 
 	tree_search(&tree, 5);
+	tree_search(&tree, 3);
 	tree_search(&tree, 4);
 	tree_search(&tree, 6);
 	tree_search(&tree, 7);
-	tree_search(&tree, 1);
 	tree_search(&tree, 2);
 	tree_search(&tree, 9);
 	tree_search(&tree, 8);
-	tree_search(&tree, 3);
 	tree_search(&tree, 0);
+	tree_search(&tree, 1);
 
 	//中序遍历
 	tree_inorder_visit(&tree, tree.root);
